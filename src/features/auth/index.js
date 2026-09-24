49e6d8bc0308f3c5ffcbd2a7f82f0f994c961844
bulsu-rggnzls <argiegonzales463@gmail.com>
2026-09-24 19:43:18 +0800

    feat: add auth feature barrel exports

---
+++ src/features/auth/index.js
@@ -0,0 +1,4 @@
+export { AuthProvider, useAuth } from './context/AuthContext'
+export { default as AuthModal } from './components/AuthModal'
+export { default as useAuthForm } from './hooks/useAuthForm'
+export { default as useAuthModalRoute } from './hooks/useAuthModalRoute'
